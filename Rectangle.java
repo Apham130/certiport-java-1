bc93bf8b0baf22eba2a245ab557d8c6b752fe326
Apham130 <andypham4747@gmail.com>
2026-05-18 18:19:20 +0000

    d

--- Rectangle.java
+++ Rectangle.java
@@ -5,14 +5,28 @@ import java.util.Scanner;
 public class Rectangle
 {
     // TO DO: Create attributes for your Rectangle class
-
+    private double length;
+    private double width;
     
     // TO DO: Create constructor(s) for your Rectangle class
-
+    public Rectangle(double l, double w) {
+        length = l;
+        width = w;
+    }
+    public Rectangle() {
+        this(1.0);
+    }
+    public Rectangle(double x) {
+        this(x,x);
+    }
 
     // TO DO: Create methods that compute the perimeter and area of your Rectangle
-
-
+    public double getArea() {
+        return length * width;
+    }
+    public double getPerimeter() {
+        return length * 2 + width * 2;
+    }
     public static void main(String[] args) throws IOException
     {
         File f = new File("input.txt");
@@ -26,10 +40,14 @@ public class Rectangle
         // TO DO: Add code that reads in a third and fourth line, which are the GPA and school name.
         // Add these fields to your input.txt
         // Print a message that displays these values
+        double GPA = Double.parseDouble(scan.nextLine());
+        String sname = scan.nextLine();
+        System.out.println("GPA: "+ GPA + "\nSchool: " + sname);
 
 
         // TO DO: Create Rectangle objects and compute their perimeter and areas
-
+        Rectangle r1 = new Rectangle(3,4);
+        System.out.printf("Rectangle r1 has a length of %1.2f%n", r1.length);
 
 
 
--- Yahyeet.java
+++ Yahyeet.java
@@ -9,12 +9,20 @@ public class Yahyeet
      */
     public static void main(String[] args) throws IOException
     {
+        File f = new File(args[0]);
+        Scanner sc = new Scanner(f);
+        int n = Integer.parseInt(args[1]);
         if (args.length >= 2)
         {
             System.out.println("You entered\nargs[0]: " + args[0] + "\nargs[1]: " + args[1]);
 
 
             // TO DO: Calculate student average scores here
+            double avg = 0;
+            for (int i = 0; i < n; i++){
+                avg += sc.nextInt();
+            }
+            System.out.println(avg/n);
         }
         else
         {
